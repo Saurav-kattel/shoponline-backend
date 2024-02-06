47f91544b43ef373e8b991c7d4aedeb9f4203633
roman thapa <thaparoman970@gmail.com>
2024-02-06 22:01:19 +0545

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const connectToMongoo=require('./db');
+const express = require('express')
+var cors=require("cors");
+
+connectToMongoo();
+const app = express()
+const port = 5000
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}));
+const admin=require("./routes/Admin")
+app.use('/api/admin',admin)
+
+
+app.listen(port, () => {
+  console.log(`inotebook backend listening on port http://localhost:${port}`)
+})
